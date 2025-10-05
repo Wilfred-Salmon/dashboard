b30cca0c5ce54dff95719f049620a8ac55079cbc
Wilfred-Salmon <Gwilfreds@gmail.com>
2025-10-05 23:06:00 +0100

    change to typescript

--- ts/line_status_loader.ts
+++ ts/line_status_loader.ts
@@ -1,13 +1,14 @@
-async function load_statuses() {
-    const status_container = document.getElementById('line-statuses');
-    const lines = window.lines;
+interface Window {
+    lines: {id: string, display_name: string}[];
+}
 
+async function load_statuses(): Promise<void> {    
     window.lines.forEach(line => {
         fetch(`line_status/${line.id}?display_name=${line.display_name}`)
         .then(res => res.text())
         .then(html => {
             const fragment = document.createRange().createContextualFragment(html);
-            document.getElementById(line.id).replaceWith(fragment);
+            document.getElementById(line.id)!.replaceWith(fragment);
         })
     });
 }
