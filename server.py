731d49aefe79959a12d953e51ee13bd44d5422b2
Wilfred-Salmon <Gwilfreds@gmail.com>
2025-10-05 12:27:47 +0100

    add tube line page

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort
+from Line import Line
 
 app = Flask(__name__)
 
@@ -6,7 +7,7 @@ app = Flask(__name__)
 @app.route('/index', methods = ['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        return redirect(url_for('different_page', name=request.form.get("some stuff")))
+        return redirect(url_for('different_page', name = request.form.get("some stuff")))
     
     return render_template('index.html')
 
@@ -14,6 +15,15 @@ def index():
 @app.route('/test/<string:name>')
 def different_page(name = None):
     return render_template('test_name.html', name=name)
+
+@app.route('/line_status/<string:line_name>')
+def line_status(line_name: str):
+    line = Line(line_name)
+    try:
+        statuses = line.get_status()
+    except Exception as e:
+        abort(404)
+    return render_template('line_status.html', line_name = line_name, line_status = statuses)
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
