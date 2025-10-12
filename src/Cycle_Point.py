4a50a79b784aa4938356f3afdd5caa5b3788136e
Wilfred-Salmon <Gwilfreds@gmail.com>
2025-10-12 11:43:23 +0100

    Add Cycle_Point class with associated API call

---
+++ src/Cycle_Point.py
@@ -0,0 +1,40 @@
+from src.ResourceCacher import ResourceCacher
+import requests
+
+class Cycle_Point_Status:
+    num_standard_bikes: int
+    num_ebikes: int
+    total_slots: int
+
+    def __init__(self, num_standard_bikes: int, num_ebikes: int, total_slots: int) -> None:
+        self.num_standard_bikes = num_standard_bikes
+        self.num_ebikes = num_ebikes
+        self.total_slots = total_slots
+
+
+class Cycle_Point(ResourceCacher[Cycle_Point_Status]):
+    display_name: str
+
+    TFL_BASE_URL = "https://api.tfl.gov.uk/BikePoint"
+    USEFUL_KEYS = {"NbStandardBikes", "NbDocks", "NbEBikes"}
+    KEY_MAPPING = {"NbStandardBikes" : "num_standard_bikes", "NbDocks": "total_slots", "NbEBikes": "num_ebikes"}
+
+    def __init__(self, id: str, display_name: str) -> None:
+        self.id = id
+        self.display_name = display_name
+        super().__init__()
+
+    def get_cycle_point_status(self) -> Cycle_Point_Status:
+        return self.get_cache()
+    
+    def get_resource_to_cache(self) -> Cycle_Point_Status:
+        url = f'{self.TFL_BASE_URL}/{self.id}'
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            raise Exception(f"Failed to fetch status for cycle point {self.id}. Error code: {response.status_code}")
+        
+        bike_point_info = response.json()["additionalProperties"]
+        trimmed_response = {self.KEY_MAPPING[dict["key"]]: dict["value"] for dict in bike_point_info if dict["key"] in self.USEFUL_KEYS}
+
+        return(Cycle_Point_Status(**trimmed_response))
